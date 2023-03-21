4056bdb3315d7ba62095843108aeeeea967dbc8c
Aelita Deinychenko <amm-aelita@ukr.net>
2023-03-21 17:20:36 +0200

    updating data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
+import Profile from 'components/profile/Profile';
+import user from 'data/user.json';
+
+import Statistics from 'components/statistics/Statistics';
+import data from 'data/data.json';
+
+import FriendList from 'components/friendList/FriendList';
+import friends from 'data/friends.json';
+
+import TransactionHistory from 'components/transactionHistory/TransationHistory';
+import transactions from 'data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
